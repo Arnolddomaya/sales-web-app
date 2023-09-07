12dbe6ce074d131abfe10a7fb2b16d292f32fdbb
Arnold Domayambaiondoum <arnold.domayambaiondoum@cdbdx.biz>
2023-09-07 16:00:57 +0200

    refactor(counter): refactor counter hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { number } from "prop-types";
-import { useState } from "react";
+import useCounter from "./useCounter";
 
 export default function Counter({ initialValue = 0, step = 1 }) {
   // TODO: begin with another
   // TODO: inc/dec by another number
-  const [counter, setCounter] = useState(initialValue);
 
-  const increment = () => setCounter(counter + step);
-  const decrement = () => setCounter(counter - step);
-
+  const [counter, { increment, decrement }] = useCounter(initialValue, step);
   return (
     <React.Fragment>
       <p>{counter}</p>
