312808b96be9e51669b0cee3820a0355a7df4608
Arnold Domayambaiondoum <arnold.domayambaiondoum@cdbdx.biz>
2023-09-08 14:55:28 +0200

    fix: building issue

---
+++ src/App/App.jsx
@@ -0,0 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainTemplate } from "../MainTemplate";
+import { HomePage } from "../home/HomePage";
+import { CountersPage } from "../counters/CountersPage";
+import "./styles.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainTemplate />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "counters",
+        element: <CountersPage />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
