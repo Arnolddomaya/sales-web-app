4db98dfd139b113fdcea90dbf04cf1fda9229323
Arnold Domayambaiondoum <arnold.domayambaiondoum@cdbdx.biz>
2023-09-08 14:35:07 +0200

    test: refactor div adding

--- src/MainTemplate/MainTemplate.jsx
+++ src/MainTemplate/MainTemplate.jsx
@@ -1,19 +1,37 @@
 import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom/dist";
+import { Link, useLocation } from "react-router-dom/dist";
+import "./styles.css";
+import { clsx } from "clsx";
 
 export default function MainTemplate() {
+  const { pathname } = useLocation();
+
+  const menu = [
+    {
+      to: "/",
+      label: "Home",
+    },
+    {
+      to: "/counters",
+      label: "Counters",
+    },
+  ];
+
   return (
     <>
       <header>
         <nav>
           <ol>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/counters">Counters</Link>
-            </li>
-            <li>Menu 3</li>
+            {menu.map(({ to, label }) => (
+              <li key={label}>
+                <Link
+                  to={to}
+                  className={clsx("menu-item", { current: to === pathname })}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ol>
         </nav>
       </header>
