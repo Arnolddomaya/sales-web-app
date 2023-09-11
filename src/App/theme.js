891d8192053ac903b6294be8ade4e125fc7c57b9
Arnold Domayambaiondoum <arnold.domayambaiondoum@cdbdx.biz>
2023-09-11 14:18:04 +0200

    feat: add Mui theme

---
+++ src/App/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+// on import les font font reboto ici pour les avoir sur l'ordinateur du lecteur
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+export const theme = createTheme({});
